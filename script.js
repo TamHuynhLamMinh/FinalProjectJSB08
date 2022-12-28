ed59507fe9fc174ba7a084d393c4de3e72c1804c
TamHuynhLamMinh <120568514+TamHuynhLamMinh@users.noreply.github.com>
2022-12-28 18:32:44 +0700

    helo

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+function myFunction() {
+    let x = document.getElementById("year").value;
+    let text;
+    if (isNaN(x) || x < 0 || x > 200) {
+      text = "Input not valid";
+    } else {
+      text = "Input OK";
+    }
+    document.getElementById("validate").innerHTML = text;
+  }
+
+function myFunction() {
+    let x = document.getElementById("year").value;
+    let text;
+    if (isNaN(x) || x < 0 || x > 9999999999) {
+      text = "Input not valid";
+    } else {
+      text = "Input OK";
+    }
+    document.getElementById("validate").innerHTML = text;
+  }
+
+function validateForm() {
+    let x = document.forms["RegisterForm"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }

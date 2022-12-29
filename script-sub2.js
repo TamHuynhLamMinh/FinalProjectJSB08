7bd09718e2d2611f4f3ff7236b4280dbe72a7f0f
TamHuynhLamMinh <120568514+TamHuynhLamMinh@users.noreply.github.com>
2022-12-29 22:44:02 +0700

    bello

--- script-sub2.js
+++ script-sub2.js
@@ -28,4 +28,22 @@ function validateForm() {
       alert("Name must be filled out");
       return false;
     }
+    let email = document.forms["RegisterForm"]["mail"].value;
+    if (email == "") {
+      alert("Email must be filled out");
+      return false;
+    }
+    if (ValidateEmail(email)== false) {
+      alert("Invalid email")
+      return false
+    } 
   }
+
+function ValidateEmail(mail) 
+{
+ if (/^[a-z][a-z0-9_\.]{5,32}@[a-z0-9]{2,}(\.[a-z0-9]{2,4}){1,2}$/.test(mail))
+  {
+    return (true)
+  }
+    return (false)
+}
